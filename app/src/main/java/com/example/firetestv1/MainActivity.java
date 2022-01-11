0c0885fb994df53674bf1224847433d1085056cd
horseman7677 <70053149+horseman7677@users.noreply.github.com>
2022-01-11 09:23:49 +0530

    configuration done

--- app/src/main/java/com/example/firetestv1/MainActivity.java
+++ app/src/main/java/com/example/firetestv1/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //successfull transmitt v1
+    //configuration done
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
